d4162993b581ce1f5bdf00b74b7a5379227f11f6
thom-window <hsbang.thom@gmail.com>
2022-07-09 16:51:40 +0900

    Initial commit

---
+++ src/main/java/com/example/mvctest/httpheaderandrestclient/HttpheaderAndRestclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvctest.httpheaderandrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpheaderAndRestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpheaderAndRestclientApplication.class, args);
+	}
+
+}
